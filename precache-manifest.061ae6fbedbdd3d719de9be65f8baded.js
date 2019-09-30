ca27385795310e6e8744f0edc220dd5513f7bcd7
geraldine low <geraldinelhy@gmail.com>
2019-09-30 11:49:59 +0700

    Updates

---
+++ precache-manifest.061ae6fbedbdd3d719de9be65f8baded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb100003d6325644bf4069db7fc16e8e",
+    "url": "/gamification/index.html"
+  },
+  {
+    "revision": "f799ca7d9cf6b4c5b769",
+    "url": "/gamification/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c6faebf97857c14e2b89",
+    "url": "/gamification/static/css/main.98347e4e.chunk.css"
+  },
+  {
+    "revision": "f799ca7d9cf6b4c5b769",
+    "url": "/gamification/static/js/2.3fdeb86c.chunk.js"
+  },
+  {
+    "revision": "c6faebf97857c14e2b89",
+    "url": "/gamification/static/js/main.92ce0975.chunk.js"
+  },
+  {
+    "revision": "0a8095d39fdb8c680543",
+    "url": "/gamification/static/js/runtime-main.e266bb48.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gamification/static/media/logo.25bf045c.svg"
+  }
+]);

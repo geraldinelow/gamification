a82b7d599379f140452355a6301fac6e0d353bd6
geraldine low <geraldinelhy@gmail.com>
2019-09-30 11:55:18 +0700

    adding text

---
+++ src/components/Product.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Product</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Product;
---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>ProductList</h1>
+            </div>
+         );
+    }
+}
+ 
+export default ProductList;
